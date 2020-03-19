c26587d82fcd55acde9c25811773cde0cf1e3d78
David Ruiz Lopez <davidruizlopez2501@gmail.com>
2020-03-19 19:38:33 +0100

    FIX - Change lat and long for a proper name

--- db/migrate/20200319183144_create_locations.rb
+++ db/migrate/20200319183144_create_locations.rb
@@ -1,8 +1,8 @@
 class CreateLocations < ActiveRecord::Migration[5.2]
   def change
     create_table :locations do |t|
-      t.float :lat
-      t.float :long
+      t.float :latitude
+      t.float :longitude
       t.string :address
 
       t.timestamps
