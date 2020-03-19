7a8048074ccbb81042c313f0ff0d50f20bab7360
David Ruiz Lopez <davidruizlopez2501@gmail.com>
2020-03-19 19:49:04 +0100

    FEAT - js map

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,12 @@
 class PagesController < ApplicationController
   def home
+    @locations = Location.geocoded #returns flats with coordinates
+
+    @markers = @flats.map do |location|
+      {
+        lat: location.latitude,
+        lng: location.longitude
+      }
   end
 
   def servicios
